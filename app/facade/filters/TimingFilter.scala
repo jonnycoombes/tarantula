d9aca88d602557434f4f900b91d7cac1dd18e658
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-09-09 22:39:22 +0100

    Documentation changes only

--- app/facade/filters/TimingFilter.scala
+++ app/facade/filters/TimingFilter.scala
@@ -9,9 +9,10 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
- *
- * @param mat
- * @param ec
+ * A simple filter that computes the server-side computation time (in ms) and then injects it into a custom header.  Useful telemetry for
+ * benchmarking and performance testing
+ * @param mat a [[akka.stream.Materializer]] instance, which could be used for stream manipulation
+ * @param ec an [[ExecutionContext]] used to performing which can be used to perform any computations OOB
  */
 class TimingFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
   lazy val log: Logger = Logger(LogNames.TimingsLogger)
