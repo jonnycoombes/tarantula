4a1cdd0062dc038b4221bfa4b4f6da8aeff27d7a
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-10-17 12:18:43 +0100

    JD-9 WSP Notification Actor

--- app/facade/actors/NotificationActor.scala
+++ app/facade/actors/NotificationActor.scala
@@ -5,12 +5,12 @@ import akka.actor.{Actor, Props}
 import com.opentext.cws.docman.{DataValue, Metadata, Node, StringValue}
 import facade.{FacadeConfig, LogNames}
 import org.joda.time.DateTime
-import play.api.libs.json.{JsObject, Json}
-import play.api.{Configuration, Logger}
-import play.utils.UriEncoding
-import play.api.libs.ws.{WSClient, WSResponse}
-import play.api.libs.ws.JsonBodyWritables._
+import play.api.Logger
 import play.api.libs.json.JodaWrites._
+import play.api.libs.json.{JsObject, Json}
+import play.api.libs.ws.JsonBodyWritables._
+import play.api.libs.ws.WSClient
+import play.utils.UriEncoding
 
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
@@ -58,8 +58,15 @@ class NotificationActor extends Actor {
 
   import NotificationActor.WSP._
 
+  /**
+   * Logger for this class
+   */
   lazy val log: Logger = Logger(LogNames.ControllerLogger)
 
+  /**
+   * Wait for an instance of [[NotifyWSP]] and then send out a notification
+   * @return
+   */
   override def receive: Receive = {
     case NotifyWSP(ws, path, node, config) =>
       log.trace(s"Received a new WSP notification request: $path")
@@ -70,6 +77,13 @@ class NotificationActor extends Actor {
   }
 
 
+  /**
+   * Does the actual notification - just fires off a HTTP request to the configured endpoint
+   * @param ws an instance of [[WSClient]]
+   * @param config the current [[FacadeConfig]]
+   * @param payload the payload to send
+   * @return
+   */
   protected def sendNotificationPayload(ws: WSClient, config: FacadeConfig, payload: JsObject) = {
     log.trace(s"Sending notification payload to endpoint: ${config.notificationEndpoint}")
     log.trace(s"Current notification payload is: $payload")
@@ -78,12 +92,23 @@ class NotificationActor extends Actor {
   }
 
 
+  /**
+   * Extracts a business partner id from a given path
+   * @param path the path to extract the business partner id from
+   * @return
+   */
   protected def extractBPIDAndPath(path: String) = {
     val elements = UriEncoding.decodePath(path, "UTF-8").split("/").toList
     (elements.head, elements.tail.mkString("/"))
   }
 
 
+  /**
+   * Constructs a [[JsObject]] containing the basic boilerplate fields required by the WSP channel. The only field completed by the
+   * facade is the business partner ID
+   * @param bpid the business partner id
+   * @return
+   */
   protected def basePayload(bpid: String): JsObject = Json.obj(
     "TemplateId" -> "",
     "TemplateName" -> "",
@@ -93,6 +118,14 @@ class NotificationActor extends Actor {
     "StatementDate" -> ""
   )
 
+  /**
+   * Builds the payload to be sent via the WSP notification channel
+   * @param node the originating [[Node]] instance
+   * @param path the path for the node
+   * @param classAttributePath the metadata path for the document class (this is specific to Discovery implementation)
+   * @param typeAttributePath the metadata path for the document type (this is specific to the Discovery implementation)
+   * @return a new [[JsObject]] containing the information relating to a new node/uploaded document
+   */
   protected def nodePayload(node: Node, path: String, classAttributePath: String, typeAttributePath: String): JsObject = {
     val elements = UriEncoding.decodePath(path, "UTF-8").split("/").toList
     Json.obj(
@@ -124,6 +157,12 @@ class NotificationActor extends Actor {
     }
   }
 
+  /**
+   * Extracts a single string value from a supplied [[Metadata]] instance, given a path
+   * @param path the path to the string value location in the format "category"."attribute"
+   * @param metadata the [[Metadata]] instance
+   * @return an [[Option[String]]] containing the value, or not
+   */
   def getSingleStringDataValue(path : String, metadata : Metadata) : Option[String] = {
     locateDataValue(path, metadata) match {
       case Some(dv) => {
